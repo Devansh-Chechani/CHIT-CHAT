a8177f357ac3db659d6c9d0b0964733c97b2d752
Devansh Chechani <chechanidevansh@gmail.com>
2024-03-30 08:29:46 +0530

    Added Message Schema

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
 import { WebSocketServer } from 'ws';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import { Message} from './models/Message.js'
 
 import authRoutes from './routes/auth.js';
 
@@ -16,95 +17,88 @@ app.use(cors({
   credentials: true // Allow sending cookies and authorization headers
 }));
 
-app.use(express.json())
-app.use(cookieParser())
- 
-app.use('/api/auth',authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
 
- const Connection = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Database Connected Successfully")
-    }
-    catch(err){
-        console.log("Error in connecting to database")
-    }
-   
- }
+const Connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connected Successfully");
+  } catch (err) {
+    console.log("Error in connecting to database");
+  }
+}
 
-      
+const server = app.listen(4000, () => {
+  Connection();
+  console.log("app is listening on port 4000");
+});
 
-const server = app.listen(4000,()=>{
-    Connection()
-    console.log("app is listening on port 4000")
-})
-
-const wss = new  WebSocketServer({server})
+const wss = new WebSocketServer({ server });
 
 const connections = new Set();
-wss.on('connection',(req,connection)=>{
-  // console.log(connection)
 
-//    req.on('message', (message) => {
-//     console.log('Received message:', message);
-//     ws.send('Server received message: ' + message); // Echo back the received message
-//   });
+wss.on('connection', (connection, req) => {
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookieString = cookies.split(';').find((str) => str.startsWith('token='));
+    if (tokenCookieString) {
+      const token = tokenCookieString.split('=')[1];
+      if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+          console.log(connection.username);
+        });
+      }
+    }
+  }
 
-   const cookies = connection.headers.cookie
-    if(cookies){
-       const tokenCookieString =  cookies.split(';').find((str)=> str.startsWith('token='))
-       if(tokenCookieString){
-          const  token = tokenCookieString.split('=')[1]
-          if(token){
-            jwt.verify( token,process.env.SECRET_KEY,{},(err,userData)=>{
-                if(err)throw err
-                
-                  const { userId, username } = userData;
-                    connection.userId = userId;
-                    connection.username = username;
-             //  console.log(connection.username)
-           })
-         }
-       }
-   }
+  connections.add(connection);
 
-     req.on('message', (message) => {
-   // console.log('Received message:', message); // Log the received message
+  connection.on('message', async (message) => {
     try {
       const messageData = JSON.parse(message.toString());
-      console.log('Parsed message:', messageData);
+      const { recipient, text } = messageData;
+      console.log(recipient);
+      if (recipient && text) {
+
+         const messageDoc = await Message.create({
+          sender:connection.userId,
+          recipient,
+          text
+         })
+
+        console.log(connection.userId);
+        [...connections].filter(c => c.userId === recipient)
+          .forEach(c => c.send(JSON.stringify({ 
+            text,
+            sender:connection.userId ,
+            recipient,
+            id:messageDoc._id
+          
+          })));
+      }
     } catch (error) {
       console.error('Error parsing message:', error);
     }
   });
 
-
-    connections.add(connection);
-   // console.log(connections)
-    updateOnlineUsers(); 
-
-   
-})
-
-
-
+  updateOnlineUsers();
+});
 
 function updateOnlineUsers() {
-    const onlineUsers = [...connections].map((c) => ({
-        userId: c.userId,
-        username: c.username
-    }));
+  const onlineUsers = [...connections].map((c) => ({
+    userId: c.userId,
+    username: c.username
+  }));
 
-   // console.log(onlineUsers)
-
-    [...wss.clients].forEach((connection) => {
-        // Check if the connection is open before sending data
-    //   console.log( connection.readyState)
-        if (connection.readyState === connection.OPEN) { // readyState 1 means the connection is open
-            connection.send(JSON.stringify({
-                online: onlineUsers
-
-            }));
-        }
-    });
+  [...connections].forEach((connection) => {
+    if (connection.readyState === connection.OPEN) {
+      connection.send(JSON.stringify({ online: onlineUsers }));
+    }
+  });
 }
